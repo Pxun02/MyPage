1999419483af1b1b7a672f2bb01359453e407c81
Pxun02 <shun.hr14@gmail.com>
2024-08-28 23:09:14 +0800

    ver2.0

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
-# Profile and mini-blog
+# MyPage
 
-## 更新について
+## 豆花更新
 - `json_updater.py`を使えば基本なんとかなるもの
    - 画像を`public/images/beancurd_images`にアップロード
    - `food.json`と`shop.json`の更新  
-
 - その他
     - 訪問済み地図の更新
     - 足りなくなったら都度`<number>.astro`を補鎮
+
+## ブログ更新
+- `/src/content/blog/<ナンバリング昇順>.md`で更新していくこと。
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,12 +1,10 @@
-// src/content/config.ts
 import { defineCollection, z } from 'astro:content';
 
 const blogCollection = defineCollection({
+  type: 'content',
   schema: z.object({
     title: z.string(),
-    pubDate: z.date(),
-    description: z.string().optional(),
-    slug: z.string(),
+    date: z.date(),
   }),
 });
 
