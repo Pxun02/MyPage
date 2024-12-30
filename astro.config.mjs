50eed1832b664d4317b41c812387b1ab5d2338c4
Pxun02 <shun.hr14@gmail.com>
2024-12-30 21:47:41 +0800

    2024 fin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 
 import react from "@astrojs/react";
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://Pxun02.github.io',
   base: "/MyPage",
-  integrations: [react()]
+  integrations: [react(), mdx()]
 });
